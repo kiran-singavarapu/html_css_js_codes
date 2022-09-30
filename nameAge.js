0ba5b0a81c31072112492deada08a51d7e6397f1
Kiransingavarapu <18pa1a05e8@vishnu.edu.in>
2022-09-30 14:10:20 +0530

    New file

---
+++ nameAge.js
@@ -0,0 +1,16 @@
+var fullName="Kiran Kumar";
+console.log(fullName);
+
+var age=22;
+
+console.log("Age : "+age);
+
+var email="singavarapukirankumar971@gmail.com"; console.log('fullName: ${fullName} age: ${age} Email: $(email)');
+
+var a=10;
+
+var b=3;
+
+var c=Math.round(a/b);
+
+console.log()
